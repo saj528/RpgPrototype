461764167a58f4e461a80853f8ee283c922b5758
ajpc <alexjoinerweb@gmail.com>
2023-04-01 10:54:01 -0500

    Create class to initialize and create animations

--- core/src/com/alexjoiner/rpgprototype/GameScreen.java
+++ core/src/com/alexjoiner/rpgprototype/GameScreen.java
@@ -29,20 +29,14 @@ public class GameScreen implements Screen {
     Rectangle playerBoundingBox;
 
 
+    SpriteProcessor spriteProcessor;
+
+    private float walkingTimer;
     private final float playerSize = 16;
     private final float WORLD_WIDTH = playerSize * 15;
     private final float WORLD_HEIGHT = playerSize * 15;
 
-    private TextureAtlas textureAtlas;
-    private TextureRegion[] playerWalkingDownTexReg, playerWalkingUpTexReg,
-            playerWalkingLeftTexReg, playerWalkingRightTexReg,
-            playerIdleUpTexReg,playerIdleDownTexReg,
-            playerIdleRightTexReg,playerIdleLeftTexReg;
-    private float playerWalkingAnimSpeed = 0.20f;
-    private float walkingTimer;
-    private Animation<TextureRegion> playerWalkingDownAnim, playerWalkingUpAnim,
-            playerWalkingRightAnim, playerWalkingLeftAnim, playerIdleDownAnim,
-    playerIdleUpAnim,playerIdleRightAnim,playerIdleLeftAnim;
+
     private Animation<TextureRegion> currentAnimation;
     private String direction;
 
@@ -50,37 +44,15 @@ public class GameScreen implements Screen {
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        spriteProcessor = new SpriteProcessor();
 
         playerBoundingBox = new Rectangle(2, 2, 16, 16);
 
-        textureAtlas = new TextureAtlas("player_atlas.atlas");
-        playerWalkingDownTexReg = createSpriteTextureRegion("walking_down", 1, 4);
-        playerWalkingUpTexReg = createSpriteTextureRegion("walking_up", 1, 4);
-        playerWalkingRightTexReg = createSpriteTextureRegion("walking_right", 1, 4);
-        playerWalkingLeftTexReg = createSpriteTextureRegion("walking_left", 1, 4);
 
-        playerWalkingDownAnim = new Animation<TextureRegion>(playerWalkingAnimSpeed, playerWalkingDownTexReg);
-        playerWalkingUpAnim = new Animation<TextureRegion>(playerWalkingAnimSpeed, playerWalkingUpTexReg);
-        playerWalkingRightAnim = new Animation<TextureRegion>(playerWalkingAnimSpeed, playerWalkingRightTexReg);
-        playerWalkingLeftAnim = new Animation<TextureRegion>(playerWalkingAnimSpeed, playerWalkingLeftTexReg);
-
-
-        playerIdleUpTexReg = new TextureRegion[1];
-        playerIdleDownTexReg = new TextureRegion[1];
-        playerIdleRightTexReg = new TextureRegion[1];
-        playerIdleLeftTexReg = new TextureRegion[1];
-        playerIdleUpTexReg[0] = textureAtlas.findRegion("walking_up4");
-        playerIdleDownTexReg[0] = textureAtlas.findRegion("walking_down4");
-        playerIdleRightTexReg[0] = textureAtlas.findRegion("walking_right4");
-        playerIdleLeftTexReg[0] = textureAtlas.findRegion("walking_left4");
-        playerIdleUpAnim = new Animation<TextureRegion>(0, playerIdleUpTexReg);
-        playerIdleDownAnim = new Animation<TextureRegion>(0, playerIdleDownTexReg);
-        playerIdleRightAnim = new Animation<TextureRegion>(0, playerIdleRightTexReg);
-        playerIdleLeftAnim = new Animation<TextureRegion>(0, playerIdleLeftTexReg);
 
         walkingTimer = 0;
 
-        currentAnimation = playerIdleDownAnim;
+        currentAnimation = spriteProcessor.getAnimations().get("playerWalkingDown");
         direction = "down";
 
         batch = new SpriteBatch();
@@ -89,14 +61,6 @@ public class GameScreen implements Screen {
     }
 
 
-    public TextureRegion[] createSpriteTextureRegion(String spriteSheetName, int startNumber, int count) {
-        TextureRegion[] tempTextureRegion = new TextureRegion[count];
-        for (int i = 0; i < count; i++) {
-            tempTextureRegion[i] = textureAtlas.findRegion(spriteSheetName + startNumber);
-            startNumber++;
-        }
-        return tempTextureRegion;
-    }
 
     @Override
     public void show() {
@@ -123,29 +87,29 @@ public class GameScreen implements Screen {
 
         if (Gdx.input.isKeyPressed(Input.Keys.W)) {
             playerBoundingBox.y += speed * delta;
-            currentAnimation = playerWalkingUpAnim;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingUp");
             direction = "up";
         }else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             playerBoundingBox.y -= speed * delta;
-            currentAnimation = playerWalkingDownAnim;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingDown");
             direction = "down";
         } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             playerBoundingBox.x -= speed * delta;
-            currentAnimation = playerWalkingLeftAnim;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingLeft");
             direction = "left";
         }else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             playerBoundingBox.x += speed * delta;
-            currentAnimation = playerWalkingRightAnim;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingRight");
             direction = "right";
         }else{
             if(direction.equals("up")){
-                currentAnimation = playerIdleUpAnim;
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleUp");
             } else if(direction.equals("down")){
-                currentAnimation = playerIdleDownAnim;
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleDown");
             } else if(direction.equals("right")){
-                currentAnimation = playerIdleRightAnim;
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleRight");
             } else if(direction.equals("left")){
-                currentAnimation = playerIdleLeftAnim;
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleLeft");
             }
         }
     }
