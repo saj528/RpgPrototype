a5896f7e5f7b98c84ef7c0c2b8acb1932637d0f6
ajpc <alexjoinerweb@gmail.com>
2023-04-01 11:27:13 -0500

    Abstracted Player into it's own class

--- core/src/com/alexjoiner/rpgprototype/GameScreen.java
+++ core/src/com/alexjoiner/rpgprototype/GameScreen.java
@@ -26,37 +26,28 @@ public class GameScreen implements Screen {
 
     private SpriteBatch batch;
 
-    Rectangle playerBoundingBox;
-
-
     SpriteProcessor spriteProcessor;
 
-    private float walkingTimer;
+    Player player;
+
+
     private final float playerSize = 16;
     private final float WORLD_WIDTH = playerSize * 15;
     private final float WORLD_HEIGHT = playerSize * 15;
 
 
-    private Animation<TextureRegion> currentAnimation;
-    private String direction;
+
 
     public GameScreen() {
 
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
         spriteProcessor = new SpriteProcessor();
-
-        playerBoundingBox = new Rectangle(2, 2, 16, 16);
-
-
-
-        walkingTimer = 0;
-
-        currentAnimation = spriteProcessor.getAnimations().get("playerWalkingDown");
-        direction = "down";
 
         batch = new SpriteBatch();
 
+        player = new Player(batch,spriteProcessor);
 
     }
 
@@ -72,47 +63,17 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.setProjectionMatrix(camera.combined);
-        walkingTimer += delta;
 
-        detectInput(delta);
+
+        player.detectInput(delta);
 
         batch.begin();
-        batch.draw(currentAnimation.getKeyFrame(walkingTimer, true), playerBoundingBox.x, playerBoundingBox.y);
+        player.renderAndUpdate(delta);
         batch.end();
 
     }
 
-    private void detectInput(float delta) {
-        float speed = 48f;
 
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            playerBoundingBox.y += speed * delta;
-            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingUp");
-            direction = "up";
-        }else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            playerBoundingBox.y -= speed * delta;
-            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingDown");
-            direction = "down";
-        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            playerBoundingBox.x -= speed * delta;
-            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingLeft");
-            direction = "left";
-        }else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            playerBoundingBox.x += speed * delta;
-            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingRight");
-            direction = "right";
-        }else{
-            if(direction.equals("up")){
-                currentAnimation = spriteProcessor.getAnimations().get("playerIdleUp");
-            } else if(direction.equals("down")){
-                currentAnimation = spriteProcessor.getAnimations().get("playerIdleDown");
-            } else if(direction.equals("right")){
-                currentAnimation = spriteProcessor.getAnimations().get("playerIdleRight");
-            } else if(direction.equals("left")){
-                currentAnimation = spriteProcessor.getAnimations().get("playerIdleLeft");
-            }
-        }
-    }
 
 
     @Override
--- core/src/com/alexjoiner/rpgprototype/Player.java
+++ core/src/com/alexjoiner/rpgprototype/Player.java
@@ -1,4 +1,78 @@
 package com.alexjoiner.rpgprototype;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+
 public class Player {
+
+    private SpriteBatch batch;
+    private SpriteProcessor spriteProcessor;
+
+    private Rectangle playerBoundingBox;
+
+    private float walkingTimer;
+
+    private Animation<TextureRegion> currentAnimation;
+
+    private String direction;
+
+
+
+    public Player(SpriteBatch batch, SpriteProcessor spriteProcessor) {
+
+        this.batch = batch;
+
+        this.spriteProcessor = spriteProcessor;
+
+        playerBoundingBox = new Rectangle(2, 2, 16, 16);
+
+        currentAnimation = spriteProcessor.getAnimations().get("playerWalkingDown");
+        direction = "down";
+
+        walkingTimer = 0;
+    }
+
+    public void renderAndUpdate(float delta){
+        walkingTimer += delta;
+        batch.draw(currentAnimation.getKeyFrame(walkingTimer, true), playerBoundingBox.x, playerBoundingBox.y);
+
+    }
+
+    public void detectInput(float delta) {
+        float speed = 48f;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            playerBoundingBox.y += speed * delta;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingUp");
+            direction = "up";
+        }else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            playerBoundingBox.y -= speed * delta;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingDown");
+            direction = "down";
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            playerBoundingBox.x -= speed * delta;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingLeft");
+            direction = "left";
+        }else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            playerBoundingBox.x += speed * delta;
+            currentAnimation = spriteProcessor.getAnimations().get("playerWalkingRight");
+            direction = "right";
+        }else{
+            if(direction.equals("up")){
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleUp");
+            } else if(direction.equals("down")){
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleDown");
+            } else if(direction.equals("right")){
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleRight");
+            } else if(direction.equals("left")){
+                currentAnimation = spriteProcessor.getAnimations().get("playerIdleLeft");
+            }
+        }
+    }
+
+
 }
